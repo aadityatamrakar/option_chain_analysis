c31ac780a80324213bc7d62aa65d50b15d699eee
Aaditya Tamrakar <aaditya.span@gmail.com>
2019-11-18 21:54:51 +0530

    code

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const fs = require('fs');
+const option_chain = require('./nse_lib');
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(express.static('public'))
+
+app.get('/', (req, res) => res.redirect('/index.html'));
+app.get('/chain', async (req, res) => {
+    try{
+        let resp = await option_chain();
+        res.send(resp);
+    }catch(err){
+        res.status(500).send(err);
+    }
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ nse_lib.js
@@ -0,0 +1,33 @@
+var request = require('request');
+
+function getOptionChain() {
+  return new Promise((resolve, reject) => {
+    var options = {
+      method: 'GET',
+      url: 'https://beta.nseindia.com/api/option-chain-indices',
+      qs: {
+        symbol: 'NIFTY'
+      },
+      headers: {
+        referer: 'https://beta.nseindia.com/get-quotes/derivatives?symbol=NIFTY&identifier=OPTIDXNIFTY21-11-2019PE11900.00',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36',
+        authority: 'beta.nseindia.com'
+      },
+      keepAlive:true,
+      json: true,
+      gzip: true,
+    };
+
+    function callback(error, response, body) {
+      if (error) console.log(error),reject(error);
+      resolve(body);
+    }
+
+    request(options, callback);
+  });
+}
+
+
+module.exports = getOptionChain;
