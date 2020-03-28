832d4945593c1f578e0087308955f889950791f4
Aaditya Tamrakar <aaditya.span@gmail.com>
2020-03-28 14:46:50 +0530

    link update

--- nse_lib.js
+++ nse_lib.js
@@ -4,25 +4,26 @@ function getOptionChain() {
   return new Promise((resolve, reject) => {
     var options = {
       method: 'GET',
-      url: 'https://beta.nseindia.com/api/option-chain-indices',
+      url: 'https://www.nseindia.com/api/option-chain-indices',
       qs: {
         symbol: 'NIFTY'
       },
       headers: {
-        referer: 'https://beta.nseindia.com/get-quotes/derivatives?symbol=NIFTY&identifier=OPTIDXNIFTY21-11-2019PE11900.00',
+        referer: 'https://www.nseindia.com/get-quotes/derivatives?symbol=NIFTY',
         'sec-fetch-mode': 'cors',
         'sec-fetch-site': 'same-origin',
         'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36',
-        authority: 'beta.nseindia.com'
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'en-IN,en;q=0.9,en-GB;q=0.8,en-US;q=0.7,hi;q=0.6,mr;q=0.5',
       },
-      keepAlive:true,
+      keepAlive: true,
       json: true,
       gzip: true,
-      proxy: "http://Selvitsswat:K9q3MrN@191.101.163.38:45785"
+      // proxy: "http://Selvitsswat:K9q3MrN@191.101.163.38:45785"
     };
 
     function callback(error, response, body) {
-      if (error) console.log(error),reject(error);
+      if (error) console.log(error), reject(error);
       resolve(body);
     }
 
