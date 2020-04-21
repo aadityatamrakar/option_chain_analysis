3b43112e7dd0b602cff9f30011cdc734382893f6
Aaditya Tamrakar <aaditya.span@gmail.com>
2020-04-22 02:29:23 +0530

    Update nse_lib.js

--- nse_lib.js
+++ nse_lib.js
@@ -19,7 +19,6 @@ function getOptionChain() {
       keepAlive: true,
       json: true,
       gzip: true,
-      // proxy: "http://Selvitsswat:K9q3MrN@191.101.163.38:45785"
     };
 
     function callback(error, response, body) {
